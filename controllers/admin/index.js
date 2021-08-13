47e114bbf4b4d138d649fd48c940d6e87dad8250
shohruh-xasanov <pfxasanov.shoxrux@gmail.com>
2021-08-13 17:56:40 +0500

    full

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -1,7 +1,18 @@
 const User = require('../../models/User')
+const Product = require('../../models/Products')
+const Order = require('../../models/Order')
 
 exports.dashboard = async (req, res, next) => {
+  const product = await Product.find().countDocuments()
+  const order = await Order.find().countDocuments()
+  const orders = await Order.find()
+  let totalSum =0
+  let totalN = 0
+  for(item of orders){
+   totalSum += item.totalPrice
+   totalN += item.totalNum
+  }
   const user = req.session.admin; 
   const admins = await User.find({role:'admin'})
-  res.render("./admin/dashboard", { layout: "./admin_layout", user,admins});
+  res.render("./admin/dashboard", { layout: "./admin_layout",product,totalN,totalSum,order, user,admins});
 };
